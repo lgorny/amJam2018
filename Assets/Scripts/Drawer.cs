8cde5145cbe2f9a3053f1a411c45009c4b68d7dc
Szymon Zachara <szymonzachara@gmail.com>
2018-07-07 01:33:54 +0200

    Working drawer

---
+++ Assets/Scripts/Drawer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour
+{
+
+    public float XMin = 0f;
+    public float XMax = 1f;
+
+    private void Awake()
+    {
+        this.gameObject.layer = LayerMask.NameToLayer("Interactable");
+    }
+
+    private void LateUpdate()
+    {
+        if (this.transform.localPosition.x < XMin)
+        {
+            this.transform.localPosition = new Vector3(XMin, transform.localPosition.y, transform.localPosition.z);
+        }
+        else if (this.transform.localPosition.x > XMax)
+        {
+            this.transform.localPosition = new Vector3(XMax, transform.localPosition.y, transform.localPosition.z);
+        }
+    }
+}
