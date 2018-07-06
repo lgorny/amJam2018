54401b056250d1917bc626f527d60aeee46f3635
Łukasz Górny <contact@lukaszgorny.pl>
2018-07-07 01:51:13 +0200

    Add SpawnManager

---
+++ Assets/Scripts/SpawnersManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnersManager : MonoBehaviour
+{
+    public List<InventoryItem> SpawnableRandomItems;
+
+    private List<SceneItemSpawner> Spawners;
+
+    void Start ()
+    {
+        Spawners = new List<SceneItemSpawner>();
+        Spawners.AddRange(GameObject.FindObjectsOfType<SceneItemSpawner>());
+
+        SpawnRandomItems(5);
+    }
+
+    public void SpawnRandomItems(int Number)
+    {
+        List<SceneItemSpawner> SpawnersTemp = new List<SceneItemSpawner>();
+        SpawnersTemp.AddRange(Spawners);
+
+        while (Number > 0 && SpawnersTemp.Count > 0)
+        {
+            int Index = Random.Range(0, SpawnersTemp.Count - 1);
+            SceneItemSpawner Spawner = SpawnersTemp[Index];
+            Spawner.Spawn(SpawnableRandomItems[Random.Range(0, SpawnableRandomItems.Count - 1)]);
+
+            SpawnersTemp.RemoveAt(Index);
+            Number--;
+        }
+    }
+}
