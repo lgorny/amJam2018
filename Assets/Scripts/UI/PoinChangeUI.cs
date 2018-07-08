f89d471eb5de4d744fb6b53b543b7de3652868c8
Łukasz Górny <contact@lukaszgorny.pl>
2018-07-08 09:11:02 +0200

    Hud

---
+++ Assets/Scripts/UI/PoinChangeUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoinChangeUI : MonoBehaviour {
+
+    public Text t;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PointChange(int change)
+    {
+        StopCoroutine("HideDel");
+
+        if (change > 0)
+        {
+            t.color = new Color(0, 1, 0, 1);
+            t.text = "+" + change.ToString();
+        }else if (change < 0)
+        {
+            t.color = new Color(1, 0, 0, 1);
+            t.text = change.ToString();
+        }
+
+        StartCoroutine("HideDel");
+    }
+
+    IEnumerator HideDel()
+    {
+        yield return new WaitForSeconds(2f);
+
+        t.color = new Color(0, 0, 0, 0);
+    }
+}
